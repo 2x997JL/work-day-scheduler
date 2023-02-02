0a5520808d214163ccb87f6f38bf2cd154b9ae7b
Jemilah Leonard <jemilah.leonard4@gmail.com>
2023-02-02 17:01:20 -0600

    added readme, fixed localstorage, edited html

--- Assets/script.js
+++ Assets/script.js
@@ -3,10 +3,11 @@ const updateTime = () => {
   document.getElementById('current-time').innerHTML = currentTime;
 
   const currentHour = dayjs().hour();
-  const timeBlocks = document.querySelectorAll('.time-block');
+  const timeBlock = document.querySelectorAll('.time-block');
 
-  timeBlocks.forEach(block => {
+  timeBlock.forEach(block => {
     const blockHour = parseInt(block.id, 10);
+
     if (blockHour < currentHour) {
       block.classList.add('past');
     } else if (blockHour === currentHour) {
@@ -19,4 +20,29 @@ const updateTime = () => {
 
 updateTime();
 setInterval(updateTime, 1000);
+
+// Save data to local storage when the save button is clicked
+const saveBtn = document.querySelectorAll('.saveBtn');
+saveBtn.forEach(button => {
+  button.addEventListener('click', event => {
+    const timeBlock = event.target.parentElement;
+    const textElement = timeBlock.querySelector('.text');
+    if (textElement) {
+      const text = textElement.value;
+      const key = `text-${dayjs().format('dddd')}-${timeBlock.id}`;
+      localStorage.setItem(key, text);
+    }
+  });
+});
+
+
+// Load data from local storage when the page loads
+const timeBlock = document.querySelectorAll('.time-block');
+timeBlock.forEach(block => {
+  const key = `text-${dayjs().format('dddd')}-${block.id}`;
+  const data = localStorage.getItem(key);
+  if (data) {
+    block.querySelector('.text').value = data;
+  }
+});
 
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# work-day-scheduler
+# Work dDy Scheduler
+
+# Introduction
+A daily planner to create a schedule that displays the current day at the top of the calendar and provides time blocks for standard business hours. Each time block is color-coded to indicate whether it is in the past, present, or future and allows the user to enter events. The saved events persist even after the page is refreshed.
+
+# Features
+1. Displays the current day at the top of the calendar
+2. Provides time blocks for standard business hours
+3. Color-coded time blocks to indicate whether it is in the past, present, or future
+4. Ability to enter events in time blocks
+5. Event text saved in local storage
+6. Saved events persist even after the page is refreshed
+
+# How to use
+1. Open the daily planner
+2. The current day will be displayed at the top of the calendar
+3. Scroll down to view the time blocks for standard business hours
+4. Click into a time block to enter an event
+5. Click the save button for that time block to save the event text
+6. Refresh the page to see the saved events persist
+
+# Conclusion
+The daily planner provides a simple and convenient way to schedule your day and keep track of events. The ability to save events in local storage ensures that your schedule is always readily available.
