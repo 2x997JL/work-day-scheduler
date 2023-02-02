fa8324e2e196309124120daaa9b4c75edc93a83c
Jemilah Leonard <jemilah.leonard4@gmail.com>
2023-02-02 12:00:15 -0600

    added js code

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,22 @@
 const updateTime = () => {
   const currentTime = dayjs().format('dddd, MMMM D YYYY, h:mm:ss a');
   document.getElementById('current-time').innerHTML = currentTime;
+
+  const currentHour = dayjs().hour();
+  const timeBlocks = document.querySelectorAll('.time-block');
+
+  timeBlocks.forEach(block => {
+    const blockHour = parseInt(block.id, 10);
+    if (blockHour < currentHour) {
+      block.classList.add('past');
+    } else if (blockHour === currentHour) {
+      block.classList.add('present');
+    } else {
+      block.classList.add('future');
+    }
+  });
 };
 
 updateTime();
 setInterval(updateTime, 1000);
+
